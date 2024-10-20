c92c3b629b015a6afaa907683f9bc067c9c3c7f6
taku10101 <taku1011ht@gmail.com>
2024-10-20 13:59:27 +0900

    bookmark crud 追加

--- src/open_api/metimo/bookmark/route.ts
+++ src/open_api/metimo/bookmark/route.ts
@@ -5,3 +5,41 @@ import {
   bookmarkRequestBody,
 } from "./schema";
 import { getRoute, postRoute, putRoute, deleteRoute } from "../../util";
+
+const BookmarkCreateRoute = postRoute({
+  path: "bookmarks",
+  requestBodySchema: bookmarkRequestBody,
+  responsesSchema: bookmarkSchema,
+});
+
+const BookmarkGetAllRoute = getRoute({
+  path: "bookmarks",
+  responsesSchema: bookmarkListResponse,
+});
+
+const BookmarkGetByIDRoute = getRoute({
+  path: "bookmarks/{bookmark_id}",
+  paramsSchema: bookmarkParams,
+  responsesSchema: bookmarkSchema,
+});
+
+const BookmarkUpdateRoute = putRoute({
+  path: "bookmarks/{bookmark_id}",
+  paramsSchema: bookmarkParams,
+  requestBodySchema: bookmarkRequestBody,
+  responsesSchema: bookmarkSchema,
+});
+
+const BookmarkDeleteRoute = deleteRoute({
+  path: "bookmarks/{bookmark_id}",
+  paramsSchema: bookmarkParams,
+  responsesSchema: bookmarkSchema,
+});
+
+export {
+  BookmarkCreateRoute,
+  BookmarkGetAllRoute,
+  BookmarkGetByIDRoute,
+  BookmarkUpdateRoute,
+  BookmarkDeleteRoute,
+};
