9f32e12b656009ee2adf3d478a782f59ae63ce17
taku10101 <taku1011ht@gmail.com>
2024-10-20 13:58:31 +0900

    attachment crud 追加

--- src/open_api/metimo/attachment/route.ts
+++ src/open_api/metimo/attachment/route.ts
@@ -5,3 +5,41 @@ import {
   attachmentRequestBody,
 } from "./schema";
 import { getRoute, postRoute, putRoute, deleteRoute } from "../../util";
+
+const AttachmentCreateRoute = postRoute({
+  path: "attachments",
+  requestBodySchema: attachmentRequestBody,
+  responsesSchema: attachmentSchema,
+});
+
+const AttachmentGetAllRoute = getRoute({
+  path: "attachments",
+  responsesSchema: attachmentListResponse,
+});
+
+const AttachmentGetByIDRoute = getRoute({
+  path: "attachments/{attachment_id}",
+  paramsSchema: attachmentParams,
+  responsesSchema: attachmentSchema,
+});
+
+const AttachmentUpdateRoute = putRoute({
+  path: "attachments/{attachment_id}",
+  paramsSchema: attachmentParams,
+  requestBodySchema: attachmentRequestBody,
+  responsesSchema: attachmentSchema,
+});
+
+const AttachmentDeleteRoute = deleteRoute({
+  path: "attachments/{attachment_id}",
+  paramsSchema: attachmentParams,
+  responsesSchema: attachmentSchema,
+});
+
+export {
+  AttachmentCreateRoute,
+  AttachmentGetAllRoute,
+  AttachmentGetByIDRoute,
+  AttachmentUpdateRoute,
+  AttachmentDeleteRoute,
+};
