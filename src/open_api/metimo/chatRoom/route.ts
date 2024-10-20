fbd1efede5d6b93cbaaa3739e92cb598fe918729
taku10101 <taku1011ht@gmail.com>
2024-10-20 14:00:26 +0900

    chatRoom crud 追加

--- src/open_api/metimo/chatRoom/route.ts
+++ src/open_api/metimo/chatRoom/route.ts
@@ -5,3 +5,41 @@ import {
   chatRoomRequestBody,
 } from "./schema";
 import { getRoute, postRoute, putRoute, deleteRoute } from "../../util";
+
+const ChatRoomCreateRoute = postRoute({
+  path: "chatRooms",
+  requestBodySchema: chatRoomRequestBody,
+  responsesSchema: chatRoomSchema,
+});
+
+const ChatRoomGetAllRoute = getRoute({
+  path: "chatRooms",
+  responsesSchema: chatRoomListResponse,
+});
+
+const ChatRoomGetByIDRoute = getRoute({
+  path: "chatRooms/{chatRoom_id}",
+  paramsSchema: chatRoomParams,
+  responsesSchema: chatRoomSchema,
+});
+
+const ChatRoomUpdateRoute = putRoute({
+  path: "chatRooms/{chatRoom_id}",
+  paramsSchema: chatRoomParams,
+  requestBodySchema: chatRoomRequestBody,
+  responsesSchema: chatRoomSchema,
+});
+
+const ChatRoomDeleteRoute = deleteRoute({
+  path: "chatRooms/{chatRoom_id}",
+  paramsSchema: chatRoomParams,
+  responsesSchema: chatRoomSchema,
+});
+
+export {
+  ChatRoomCreateRoute,
+  ChatRoomGetAllRoute,
+  ChatRoomGetByIDRoute,
+  ChatRoomUpdateRoute,
+  ChatRoomDeleteRoute,
+};
